977b0b411e8f333f3b10bbf4b453b0d7d0d284e2
josemarsilva <josemar.silva@inmetrics.com.br>
2018-03-12 00:19:01 -0300

    ExcelToJson

--- src/test/java/org/gnu/automation/cucumber_bdd_se/AppTest.java
+++ src/test/java/org/gnu/automation/cucumber_bdd_se/AppTest.java
@@ -4,6 +4,13 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.gnu.automation.cucumber_bdd_se.util.ExcelToJson;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
 /**
  * Unit test for simple App.
  */
@@ -33,6 +40,22 @@ public class AppTest
      */
     public void testApp()
     {
+    	
         assertTrue( true );
     }
+    
+    public void testExcelToJson() throws Exception
+    {
+    	System.out.println("testExcelToJson()");
+    	
+//    	ExcelToJson excelToJsonTestData = new ExcelToJson(".\\src\\main\\resources\\testdata\\crmpro\\TestData.xlsx");
+    	ExcelToJson excelToJsonConfigLayout = new ExcelToJson(".\\src\\main\\resources\\testdata\\crmpro\\ConfigLayout.xlsx");
+//    	ExcelToJson excelToJsonConfigCredencial = new ExcelToJson(".\\src\\main\\resources\\testdata\\crmpro\\ConfigCredencial.xlsx");
+    	
+    	System.out.println("end.");
+
+    	assertTrue( true );
+    }
+
+    
 }
